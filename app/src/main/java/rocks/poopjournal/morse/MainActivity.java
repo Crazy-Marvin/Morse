19769b6ee0de7d7d563e61c5ef72a6b3f2111b69
Fahad Saleem <fsaleem.bese15seecs@seecs.edu.pk>
2020-05-25 23:50:31 +0500

    added digits support

--- app/src/main/java/rocks/poopjournal/morse/MainActivity.java
+++ app/src/main/java/rocks/poopjournal/morse/MainActivity.java
@@ -853,6 +853,26 @@ public class MainActivity extends AppCompatActivity implements Camera.AutoFocusC
                 return "--..";
             case " ":
                 return  " ";
+            case "0":
+                return "-----";
+            case "1":
+                return ".----";
+            case "2":
+                return "..---";
+            case "3":
+                return "...--";
+            case "4":
+                return "....-";
+            case "5":
+                return ".....";
+            case "6":
+                return "-....";
+            case "7":
+                return "--...";
+            case "8":
+                return "---..";
+            case "9":
+                return "----.";
         }
         return "";
     }
@@ -916,6 +936,26 @@ public class MainActivity extends AppCompatActivity implements Camera.AutoFocusC
                 return "z";
             case "   ":
                 return " ";
+            case "-----":
+                return "0";
+            case ".----":
+                return "1";
+            case "..---":
+                return "2";
+            case "...--":
+                return "3";
+            case "....-":
+                return "4";
+            case ".....":
+                return "5";
+            case "-....":
+                return "6";
+            case "--...":
+                return "7";
+            case "---..":
+                return "8";
+            case "----.":
+                return "9";
         }
 
         return "";
@@ -1105,5 +1145,6 @@ public class MainActivity extends AppCompatActivity implements Camera.AutoFocusC
         arrayList = helper.getAllPhrases();
         // Checks the orientation of the scree
         }
+
 
 }
