5d804123a882fe612b299850725136675a5b9241
mubeen <mubeen1519@gmail.com>
2025-09-24 17:41:25 +0500

    added arabic,urdu,hindi, and other latin words support, and handled edge to edge display

--- app/src/main/java/rocks/poopjournal/morse/PhraseBookActivity.java
+++ app/src/main/java/rocks/poopjournal/morse/PhraseBookActivity.java
@@ -2,8 +2,13 @@ package rocks.poopjournal.morse;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,13 +20,22 @@ public class PhraseBookActivity extends AppCompatActivity {
     RecyclerView phrasebookRv;
     PhrasebookAdapter phrasebookAdapter;
     DBHelper helper;
+    RelativeLayout mainContainer;
     ImageView back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrasebook);
+        mainContainer = findViewById(R.id.main_container);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
+        // Apply insets padding to avoid notch / status bar / nav bar overlap
+        ViewCompat.setOnApplyWindowInsetsListener(mainContainer, (view, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
         helper = new DBHelper(getApplicationContext());
 
         back = findViewById(R.id.back);
